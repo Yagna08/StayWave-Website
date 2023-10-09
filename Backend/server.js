5d7b9a0d2f2e73830cd2d7f3e0ff10787881a83e
Yagna08 <yagnapatel2108@gmail.com>
2023-10-09 22:41:20 +0530

    Added Server is Running

--- Backend/server.js
+++ Backend/server.js
@@ -21,6 +21,9 @@ app.use(morgan('dev'))
 
 app.use('https://staywave-backend.onrender.com/api/user',require('./Routing/userrouting'))
 //listening port
+app.post('/', (req, res) => {
+    res.send('Server is Running')
+})
 app.listen(port,()=>
 {
     console.log(`Server running ${process.env.NODE_MODE} on ${process.env.PORT}`)
